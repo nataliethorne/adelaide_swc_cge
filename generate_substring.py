11200825bb1d9fd39a5982c92bfd097bf4f86349
Nathan S. Watson-Haigh <nathan@watsonhaigh.net>
2013-09-26 14:12:33 +0930

    rejig generate_substring()

--- generate_substring.py
+++ generate_substring.py
@@ -1,16 +1,13 @@
-from random import randint
+from random import randint,seed
 
-def random():
-  return 2
-  pass
-
-def generate_substring(sequence, length=100, random_start_position=random()):
+def generate_substring(sequence, length=100):
   '''Given an input string, return a random substring of length length (default 100)'''
-  #random_start_position = random() # from 0 to len(sequence)
-  # end position needs to account for circular nature of the sequence
-  end_position = random_start_position + length
-  return sequence[random_start_position:end_position]
-  pass
+  start_idx = randint(0,len(sequence))
+  end_idx = start_idx + length
+  return get_substring_from_circular(sequence, start_idx, end_idx)
+
+def get_substring_from_circular(sequence, start_idx, end_idx):
+  return sequence[start_idx:end_idx]
 
 def read_simulator(sequence, n, out_fasta_filename):
 	# Call generate_substring() n number of times
@@ -37,4 +34,4 @@ def enumerate_kmers_in_fasta_file(k_mer_counts):
 #mu_only = set(my_mu_dict.keys()) - set(my_wt_dict.keys())
 #wt_only = set(my_wt_dict.keys()) - set(my_mu_dict.keys())
 
-print generate_substring('ATAGATAG', 2, 5)
+
