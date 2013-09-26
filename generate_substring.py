0c3b5e0a892c4366c523328961f269a1bdf9f7c4
Nathan S. Watson-Haigh <nathan@watsonhaigh.net>
2013-09-26 13:32:24 +0930

    Updated file so it at least runs without errors.

--- generate_substring.py
+++ generate_substring.py
@@ -1,9 +1,16 @@
-def generate_substring(sequence, length=100):
-	'''Given an input string, return a random substring of length length (default 100)'''
-    random_start_position = random() # from 0 to len(sequence)
-	# end position needs to account for circular nature of the sequence
-	#substring = sequence[random_start_position:]
-	pass
+from random import randint
+
+def random():
+  return 2
+  pass
+
+def generate_substring(sequence, length=100, random_start_position=random()):
+  '''Given an input string, return a random substring of length length (default 100)'''
+  #random_start_position = random() # from 0 to len(sequence)
+  # end position needs to account for circular nature of the sequence
+  end_position = random_start_position + length
+  return sequence[random_start_position:end_position]
+  pass
 
 def read_simulator(sequence, n, out_fasta_filename):
 	# Call generate_substring() n number of times
@@ -23,9 +30,11 @@ def enumerate_kmers_in_fasta_file(k_mer_counts):
 	
 	pass
 
-my_wt_dict = enumerate_kmers_in_fasta_file(wt_reads)
-my_mu_dict = enumerate_kmers_in_fasta_file(mu_reads)
+#my_wt_dict = enumerate_kmers_in_fasta_file(wt_reads)
+#my_mu_dict = enumerate_kmers_in_fasta_file(mu_reads)
 
 # find differences between my_wt_dict and my_mu_dict
-mu_only = set(my_mu_dict.keys()) - set(my_wt_dict.keys())
-wt_only = set(my_wt_dict.keys()) - set(my_mu_dict.keys())
+#mu_only = set(my_mu_dict.keys()) - set(my_wt_dict.keys())
+#wt_only = set(my_wt_dict.keys()) - set(my_mu_dict.keys())
+
+print generate_substring('ATAGATAG', 2, 5)
